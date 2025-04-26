57fc60526738857924e46a240e6731436ff1b7fb
tetsuhiro-dayo <119085878+tetuhiro-dayo@users.noreply.github.com>
2025-04-26 19:55:25 +0900

    機能追加：パイプラインと非同期実行

--- main.c
+++ main.c
@@ -19,84 +19,134 @@
 #define COLOR_GREEN "\033[32m"
 #define COLOR_RED "\033[31m"
 
+pid_t bg_pids[MAX_ARGS];
+int bg_count = 0;
+char history[MAX_HISTORY][MAX_CMD_LENGTH];
+int history_count = 0;
+
 int isCommand(const char *input, const char *command) {
     return strcmp(input, command) == 0;
 }
 
-int main() {
-    char command[MAX_CMD_LENGTH] = {0};
-    char cwd[MAX_PATH_LENGTH];
-    char *args[MAX_ARGS];
-    char history[MAX_HISTORY][MAX_CMD_LENGTH];
-    int history_count = 0;
-    struct termios original_termios;
+void executeCommandWithMultiplePipes(char *command) {
+    char *cmds[MAX_ARGS];
+    int cmd_count = 0;
+    int background = 0;
 
-    disableCanonicalMode(&original_termios);
-    srand(time(NULL));
+    // バックグラウンド実行チェック
+    char *bg_check = strchr(command, '&');
+    if (bg_check != NULL) {
+        background = 1;
+        *bg_check = '\0';
+    }
+
+    char *token = strtok(command, "|");
+    while (token != NULL && cmd_count < MAX_ARGS - 1) {
+        cmds[cmd_count++] = token;
+        token = strtok(NULL, "|");
+    }
+    cmds[cmd_count] = NULL;
+
+    int pipefd[2 * (cmd_count - 1)];
+    for (int i = 0; i < cmd_count - 1; i++) {
+        if (pipe(pipefd + i*2) < 0) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < cmd_count; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (i != 0) {
+                dup2(pipefd[(i-1)*2], STDIN_FILENO);
+            }
+            if (i != cmd_count - 1) {
+                dup2(pipefd[i*2 + 1], STDOUT_FILENO);
+            }
+
+            for (int j = 0; j < 2 * (cmd_count - 1); j++) {
+                close(pipefd[j]);
+            }
+
+            char *args[MAX_ARGS];
+            int k = 0;
+            char *arg = strtok(cmds[i], " ");
+            while (arg != NULL && k < MAX_ARGS - 1) {
+                args[k++] = arg;
+                arg = strtok(NULL, " ");
+            }
+            args[k] = NULL;
+
+            if (i == cmd_count - 1) {
+                for (int m = 0; args[m] != NULL; m++) {
+                    if (strcmp(args[m], ">") == 0 || strcmp(args[m], ">>") == 0) {
+                        int append = (strcmp(args[m], ">>") == 0);
+                        char *filename = args[m + 1];
+                        int fd = open(filename, O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC), 0644);
+                        if (fd < 0) {
+                            perror("open");
+                            exit(EXIT_FAILURE);
+                        }
+                        dup2(fd, STDOUT_FILENO);
+                        close(fd);
+                        args[m] = NULL;
+                        break;
+                    }
+                }
+            }
+
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else if (background) {
+            bg_pids[bg_count++] = pid;
+        }
+    }
+
+    for (int i = 0; i < 2 * (cmd_count - 1); i++) {
+        close(pipefd[i]);
+    }
+
+    if (!background) {
+        for (int i = 0; i < cmd_count; i++) {
+            wait(NULL);
+        }
+    }
+}
+
+void waitForBackground() {
+    for (int i = 0; i < bg_count; i++) {
+        waitpid(bg_pids[i], NULL, 0);
+    }
+    bg_count = 0;
+}
+
+// ここから main 関数
+int main() {
+    char command[MAX_CMD_LENGTH];
 
     while (1) {
-        if (getcwd(cwd, sizeof(cwd)) == NULL) {
-            fprintf(stderr, COLOR_RED);
-            perror("getcwd");
-            fprintf(stderr, COLOR_RESET);
-            continue;
-        }
-
-        printf(COLOR_CYAN "[ tossh : %s ]$ " COLOR_RESET, cwd);
+        printf(COLOR_CYAN "[ tossh ]$ " COLOR_RESET);
         fflush(stdout);
 
-        int pos = 0;
-        memset(command, 0, sizeof(command));
-
-        while (1) {
-            char c;
-            if (read(STDIN_FILENO, &c, 1) <= 0) {
-                continue;
-            }
-
-            if (c == '\n') {
-                printf("\n");
-                break;
-            } else if (c == 127 || c == 8) {
-                if (pos > 0) {
-                    pos--;
-                    command[pos] = '\0';
-                    printf("\b \b");
-                    fflush(stdout);
-                }
-            } else {
-                if (pos < MAX_CMD_LENGTH - 1) {
-                    command[pos++] = c;
-                    printf("%c", c);
-                    fflush(stdout);
-                }
-            }
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break;
         }
+
+        command[strcspn(command, "\n")] = 0;
 
         if (strlen(command) > 0) {
             strncpy(history[history_count % MAX_HISTORY], command, MAX_CMD_LENGTH);
             history_count++;
         }
 
-        if (strncmp(command, "alias ", 6) == 0) {
-            registerAlias(command + 6);
-            continue;
-        }
-
-        expandAlias(command);
-
         if (isCommand(command, "exit") || isCommand(command, "quit") || isCommand(command, "bye")) {
-            printf(COLOR_GREEN "Good Bye." COLOR_RESET "\n");
             break;
         }
 
-        if (strncmp(command, "cd ", 3) == 0) {
-            char *path = command + 3;
-            if (chdir(path) != 0) {
-                fprintf(stderr, COLOR_RED);
-                perror("cd");
-                fprintf(stderr, COLOR_RESET);
-            }
+        if (isCommand(command, "wait")) {
+            waitForBackground();
             continue;
         }
 
@@ -106,12 +156,7 @@ int main() {
         }
 
         if (isCommand(command, "ver")) {
-            printf("tossh version 1.0.0\n");
-            continue;
-        }
-
-        if (isCommand(command, "time")) {
-            system("date");
+            printf("tossh version 1.2.0\n");
             continue;
         }
 
@@ -146,70 +191,8 @@ int main() {
             continue;
         }
 
-        int i = 0;
-        char *token = strtok(command, " ");
-        char *outfile = NULL;
-        char *infile = NULL;
-        int append_mode = 0;
-        while (token != NULL && i < MAX_ARGS - 1) {
-            if (strcmp(token, ">") == 0 || strcmp(token, ">>") == 0) {
-                append_mode = (strcmp(token, ">>") == 0);
-                token = strtok(NULL, " ");
-                if (token != NULL) {
-                    outfile = token;
-                }
-                break;
-            } else if (strcmp(token, "<") == 0) {
-                token = strtok(NULL, " ");
-                if (token != NULL) {
-                    infile = token;
-                }
-                break;
-            }
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
-
-        pid_t pid = fork();
-        if (pid == 0) {
-            if (outfile) {
-                int flags = O_WRONLY | O_CREAT | (append_mode ? O_APPEND : O_TRUNC);
-                int fd = open(outfile, flags, 0644);
-                if (fd < 0) {
-                    fprintf(stderr, COLOR_RED);
-                    perror("open outfile");
-                    fprintf(stderr, COLOR_RESET);
-                    exit(EXIT_FAILURE);
-                }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-            }
-            if (infile) {
-                int fd = open(infile, O_RDONLY);
-                if (fd < 0) {
-                    fprintf(stderr, COLOR_RED);
-                    perror("open infile");
-                    fprintf(stderr, COLOR_RESET);
-                    exit(EXIT_FAILURE);
-                }
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-            }
-            execvp(args[0], args);
-            fprintf(stderr, COLOR_RED);
-            perror("execvp");
-            fprintf(stderr, COLOR_RESET);
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            wait(NULL);
-        } else {
-            fprintf(stderr, COLOR_RED);
-            perror("fork");
-            fprintf(stderr, COLOR_RESET);
-        }
+        executeCommandWithMultiplePipes(command);
     }
 
-    restoreCanonicalMode(&original_termios);
     return 0;
 }
